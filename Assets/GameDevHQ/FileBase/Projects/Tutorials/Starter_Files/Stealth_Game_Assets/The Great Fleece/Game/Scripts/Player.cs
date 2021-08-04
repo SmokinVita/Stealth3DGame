7cd43f40d137d54902ae7a13c78f7828403bb6c5
SmokinVita <69760636+SmokinVita@users.noreply.github.com>
2021-08-04 17:30:06 +0900

    Got camera's set up and object spawns where clicked.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //if left click
+        //cast a ray from mouse pos
+        //debug the floor position hit
+        //create object at floor position
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out RaycastHit hit, 100))
+            {
+                Debug.Log($"We clicked at {hit.point}");
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = hit.point;
+            }
+        }
+    }
+}
