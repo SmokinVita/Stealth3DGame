cc0cddd5a099e651e055706c685f7041d89a3ef3
SmokinVita <69760636+SmokinVita@users.noreply.github.com>
2021-08-16 20:59:56 +0900

    Added Security camera and sleeping guard logic

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Stealth_Game_Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _cutScene;
+    [SerializeField]
+    private Animator _animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            
+            Renderer _render = GetComponent<Renderer>();
+            Color color = new Color(.6f, .1f, .1f, .3f);
+            _render.material.SetColor("_TintColor", color);
+            _animator.enabled = false;
+            StartCoroutine(DelayCutsceneRoutine());
+            
+        }
+    }
+
+    private IEnumerator DelayCutsceneRoutine()
+    {
+        yield return new WaitForSeconds(.5f);
+        _cutScene.SetActive(true);
+    }
+}
